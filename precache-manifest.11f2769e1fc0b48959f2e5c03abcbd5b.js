3c6350a956fd223d41ee6734b5e9a95c40cd6a66
Matt McAllen <matt.mcallen@burberry.com>
2020-07-04 16:14:07 +0100

    Updates

---
+++ precache-manifest.11f2769e1fc0b48959f2e5c03abcbd5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505ad8c3e796330ff9aabe4940a4dbbe",
+    "url": "/powerpuff_index2/index.html"
+  },
+  {
+    "revision": "810e0fdc30f315545573",
+    "url": "/powerpuff_index2/static/css/main.b88ce3b0.chunk.css"
+  },
+  {
+    "revision": "acb0e2d802e5d1f2e320",
+    "url": "/powerpuff_index2/static/js/2.977dd8e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/powerpuff_index2/static/js/2.977dd8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810e0fdc30f315545573",
+    "url": "/powerpuff_index2/static/js/main.d584b6a4.chunk.js"
+  },
+  {
+    "revision": "b745fd9775c40b56f604",
+    "url": "/powerpuff_index2/static/js/runtime-main.a40f8d9b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/powerpuff_index2/precache-manifest.11f2769e1fc0b48959f2e5c03abcbd5b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/powerpuff_index2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
